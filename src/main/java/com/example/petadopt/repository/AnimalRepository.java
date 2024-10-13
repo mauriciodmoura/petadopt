88ab7a8b85afb45ffea9b99f5c51d68db42cfae3
mauriciomoura-calcme <mauricio.moura@calcme.com.br>
2024-10-13 15:36:21 -0300

    feat: bean repo

--- src/main/java/com/example/petadopt/repository/AnimalRepository.java
+++ src/main/java/com/example/petadopt/repository/AnimalRepository.java
@@ -3,8 +3,11 @@ package com.example.petadopt.repository;
 import com.example.petadopt.model.Animal;
 import com.example.petadopt.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
     List<Animal> findByStatus(Status status);
 }
--- src/main/java/com/example/petadopt/service/impl/AnimalServiceImpl.java
+++ src/main/java/com/example/petadopt/service/impl/AnimalServiceImpl.java
@@ -3,12 +3,18 @@ package com.example.petadopt.service.impl;
 import com.example.petadopt.model.Animal;
 import com.example.petadopt.repository.AnimalRepository;
 import com.example.petadopt.service.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnimalServiceImpl implements AnimalService{
 
     private AnimalRepository animalRepository;
+
+    @Autowired
+    public AnimalServiceImpl(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
 
     @Override
     public Animal create(Animal animal) {
